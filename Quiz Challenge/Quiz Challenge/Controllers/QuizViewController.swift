8206b920fc7bcaf9a017e2d123af6bcdb0c624c3
Joao Vitor P. Moraes <jvlppm@gmail.com>
2020-02-15 12:59:42 -0300

    Adapting layout to virtual keyboard

--- Quiz Challenge/Quiz Challenge/Controllers/QuizViewController.swift
+++ Quiz Challenge/Quiz Challenge/Controllers/QuizViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    @IBOutlet weak var bottomPanelPositionConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardChangeNotification), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(onKeyboardChangeNotification), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
 
+    @objc func onKeyboardChangeNotification(notification: Notification) {
+        guard
+            let frameData = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey],
+            let keyboardScreenEndFrame = (frameData as? NSValue)?.cgRectValue
+        else {
+            return
+        }
 
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        let keyboardHeight = keyboardViewEndFrame.height - view.safeAreaInsets.bottom
+
+        self.onKeyboardUpdate(height: keyboardHeight)
+    }
+
+    func onKeyboardUpdate(height: CGFloat) {
+        bottomPanelPositionConstraint.constant = height
+        view.layoutIfNeeded()
+    }
 }
 
